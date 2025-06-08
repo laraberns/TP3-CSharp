30ba4ecd9e92e575b4b1891879c637140f8a32bd
Lara Pereira <larabernspereira@gmail.com>
2025-06-08 18:05:30 -0300

    Add project files.

---
+++ Data/CityBreaksContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CityBreaks.Web.Data
+{
+    public class CityBreaksContext : DbContext
+    {
+        public CityBreaksContext(DbContextOptions<CityBreaksContext> options) : base(options) { }
+        DbSet<Country> Countries { get; set; }
+    }
+}
